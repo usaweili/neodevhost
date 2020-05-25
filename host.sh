d089742bf6bccb677bcec15ac02e7da1b2fda2ce
Fusion <leemenhin@gmail.com>
2020-05-25 11:47:24 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -76,7 +76,6 @@ sed -i 's/^.//' tmphost
 sed -i 's/^[ \t]*//;s/[ \t]*$//' tmphost
 sed -i '/^$/d' tmphost
 sed -i 's/0.0.0.0//' tmphost
-sed -i 's/-//' tmphost
 sed -i 's/0.0.0.0.//' tmphost
 sort -u tmphost > host
 sed -i '1d' host
