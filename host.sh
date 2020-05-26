e9128775e4e2dd0779456eb8cb8b8913148796bc
Fusion <leemenhin@gmail.com>
2020-05-27 03:42:35 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -61,16 +61,13 @@ for url in `cat black` ;do
     cat tmp >> tmphost 
     rm tmp
 done
-sed -i '/\$/d' tmphost  #temp fix
-sed -i '/\?/d' tmphost  #temp fix
 sed -i '/]/d' tmphost
 sed -i '/:/d' tmphost
 sed -i '/#/d' tmphost
 sed -i '/ɢ/d' tmphost
 sed -i '/255.255.255.255/d' tmphost
 sed -i '/ip6-/d' tmphost
-sed -i '/local/d' tmphost
-sed -i '/,/d' tmphost #temp fix 
+sed -i '/local/d' tmphost 
 sed -i 's/127.0.0.1 //' tmphost
 sed -i 's/0.0.0.0.//' tmphost
 sed -i 's/0.0.0.0//' tmphost
