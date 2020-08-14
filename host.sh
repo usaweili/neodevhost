0b868dfe5927368788ab6e9536593dc530d466b0
Fusion <leemenhin@gmail.com>
2020-08-15 05:43:07 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -86,6 +86,7 @@ sed -i '/#/d' tmpblock
 sed -i '/255.255.255.255/d' tmpblock
 sed -i '/ip6-/d' tmpblock
 sed -i '/local/d' tmpblock
+sed -i 's/0.0.0.0 //' tmpblock
 sed -i 's/0.0.0.0//' tmpblock
 sed -i 's/:://' tmpblock
 sed -i 's/^\.//' tmpblock
